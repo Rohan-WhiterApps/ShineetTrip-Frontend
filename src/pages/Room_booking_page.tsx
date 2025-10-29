ec66c2c1e4ad4d86754f307ee78bd75ab740ac19
rohan <rohanthakur89768@gmail.com>
2025-10-29 21:00:00 +0530

    added codes for booking

--- src/pages/Room_booking_page.tsx
+++ src/pages/Room_booking_page.tsx
@@ -40,7 +40,7 @@ const rooms: Room[] = [
     options: [
       {
         title: "Room Only",
-        originalPrice: 3090,
+        originalPrice: 3790,
         discountedPrice: 2389,
         savingAmount: 1225,
         description: "Standard Rate"
